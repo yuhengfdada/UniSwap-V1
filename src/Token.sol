97967395c8ddd915b37469d9c4199175bcfb2ca0
yuhengfdada <877751954@qq.com>
2025-01-17 11:12:52 +0800

    update spdx

--- src/Token.sol
+++ src/Token.sol
@@ -1,10 +1,10 @@
+// SPDX-License-Identifier: MIT
 pragma solidity ^0.8.0;
 
 import {ERC20} from "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 
-
 contract Token is ERC20 {
-    constructor (string memory name, string memory symbol, uint initialSupply) ERC20(name, symbol) {
+    constructor(string memory name, string memory symbol, uint256 initialSupply) ERC20(name, symbol) {
         _mint(msg.sender, initialSupply);
     }
 }
